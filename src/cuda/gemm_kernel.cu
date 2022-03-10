368fa36340e6c3b753d40df61f9977f08d61ea29
apuaaChen <chenzd15thu@gmail.com>
2022-03-10 14:46:35 -0800

    [mma] add iterator to code base

--- src/cuda/gemm_kernel.cu
+++ src/cuda/gemm_kernel.cu
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <vector>
 #include "cuda_bf16.h"
+#include "mma/default_mma.h"
 
 
 using ThreadblockShape_bf16 = cutlass::gemm::GemmShape<128, 128, 64>;
@@ -27,7 +28,13 @@ using EpilogueOp_bf16 = cutlass::epilogue::thread::LinearCombination<
 constexpr int NumStages = 3;
 
 // Mma
-using Mma_bf16_nnn = typename cutlass::gemm::threadblock::DefaultMma<
+// using Mma_bf16_nnn = typename cutlass::gemm::threadblock::DefaultMma<
+//     cutlass::bfloat16_t, cutlass::layout::RowMajor, 128 / cutlass::sizeof_bits<cutlass::bfloat16_t>::value,
+//     cutlass::bfloat16_t, cutlass::layout::RowMajor, 128 / cutlass::sizeof_bits<cutlass::bfloat16_t>::value,
+//     float, cutlass::layout::RowMajor, cutlass::arch::OpClassTensorOp, cutlass::arch::Sm80,
+//     ThreadblockShape_bf16, WarpShape_bf16, InstructionShape_bf16, NumStages, cutlass::arch::OpMultiplyAdd>::ThreadblockMma;
+
+using Mma_bf16_nnn = typename cutlass::gemm::threadblock::DefaultMmaV2<
     cutlass::bfloat16_t, cutlass::layout::RowMajor, 128 / cutlass::sizeof_bits<cutlass::bfloat16_t>::value,
     cutlass::bfloat16_t, cutlass::layout::RowMajor, 128 / cutlass::sizeof_bits<cutlass::bfloat16_t>::value,
     float, cutlass::layout::RowMajor, cutlass::arch::OpClassTensorOp, cutlass::arch::Sm80,
@@ -43,6 +50,128 @@ union SharedStorage_bf16_nnn {
     typename Mma_bf16_nnn::SharedStorage main_loop;
     typename Epilogue_bf16_nnn::SharedStorage epilogue;
 };
+
+template<typename _Mma, typename _SharedStorage, typename _Epilogue>
+__global__ void cutlassGemmKernel_bf16_gold(
+    cutlass::gemm::GemmCoord problem_size,
+    cutlass::gemm::GemmCoord grid_tiled_shape,
+    typename _Mma::IteratorA::Params params_A,
+    cutlass::bfloat16_t* __restrict__ ptr_A,
+    typename _Mma::IteratorB::Params params_B,
+    cutlass::bfloat16_t* __restrict__ ptr_B,
+    typename _Epilogue::OutputTileIterator::Params params_D,
+    cutlass::bfloat16_t* __restrict__ ptr_D,
+    typename _Epilogue::OutputOp::Params output_op_,
+    int gemm_k_size)
+{
+    extern __shared__ int SharedStorageBase[];
+
+    _SharedStorage& shared_storage = *reinterpret_cast<_SharedStorage *>(SharedStorageBase);
+
+    ThreadblockSwizzle threadblock_swizzle;
+
+    cutlass::gemm::GemmCoord threadblock_tile_offset=threadblock_swizzle.get_tile_offset(grid_tiled_shape);
+
+    // Early exit if CTA is out of range
+    if (grid_tiled_shape.m() <= threadblock_tile_offset.m() ||
+        grid_tiled_shape.n() <= threadblock_tile_offset.n())
+    {
+        return;
+    }
+
+    // Compute initial location in logical coordinates
+    cutlass::MatrixCoord tb_offset_A{
+        threadblock_tile_offset.m() * _Mma::Shape::kM,
+        threadblock_tile_offset.k() * gemm_k_size
+    };
+
+    cutlass::MatrixCoord tb_offset_B{
+        threadblock_tile_offset.k() * gemm_k_size,
+        threadblock_tile_offset.n() * _Mma::Shape::kN
+    };
+
+    // Problem size
+    int problem_size_k = min(problem_size.k(), (threadblock_tile_offset.k() + 1) * gemm_k_size);
+
+    int gemm_k_iterations = (problem_size_k - tb_offset_B.row() + _Mma::Shape::kK - 1) / _Mma::Shape::kK;
+
+    // Compute position within threadblock
+    int thread_idx = threadIdx.x;
+
+    // Construct iterators to A, B, and E operands
+    typename _Mma::IteratorA iterator_A(
+        params_A,
+        //ref_A.data(),
+        ptr_A,
+        {problem_size.m(), problem_size_k},
+        thread_idx,
+        tb_offset_A
+    );
+
+    typename _Mma::IteratorB iterator_B(
+        params_B,
+        //ref_B.data(),
+        ptr_B,
+        {problem_size_k, problem_size.n()},
+        thread_idx,
+        tb_offset_B
+    );
+
+    // Broadcast the warp_id computed by lane 0 to ensure dependent code
+    // is compuled as warp-uniform
+    int warp_idx = __shfl_sync(0xffffffff, threadIdx.x / 32, 0);
+    int lane_idx = threadIdx.x % 32;
+
+    //
+    //  Main loop
+    //
+
+    // Construct thread-scoped matrix multiply
+    _Mma mma(shared_storage.main_loop, thread_idx, warp_idx, lane_idx);
+
+    typename _Mma::FragmentC accumulators;
+
+    accumulators.clear();
+
+    if (gemm_k_iterations > 0){
+        mma(gemm_k_iterations, accumulators, iterator_A, iterator_B, accumulators);
+    }
+
+    //
+    //  Epilogue
+    //
+
+    typename _Epilogue::OutputOp output_op(output_op_);
+
+    threadblock_tile_offset = threadblock_swizzle.get_tile_offset(grid_tiled_shape);
+
+    // (blockIdx.x * TileM, blockIdx.y * TileN)
+    cutlass::MatrixCoord threadblock_offset(
+        threadblock_tile_offset.m() * _Mma::Shape::kM,
+        threadblock_tile_offset.n() * _Mma::Shape::kN
+    );
+
+    int block_idx = threadblock_tile_offset.m() + threadblock_tile_offset.n() * grid_tiled_shape.m();
+    
+    typename _Epilogue::OutputTileIterator iterator_D(
+        params_D,
+        ptr_D,
+        problem_size.mn(),
+        thread_idx,
+        threadblock_offset
+    );
+
+    
+    _Epilogue epilogue(
+        shared_storage.epilogue,
+        thread_idx,
+        warp_idx,
+        lane_idx
+    );
+
+    epilogue(output_op, iterator_D, accumulators, iterator_D);
+}
+
 
 template<typename _Mma, typename _SharedStorage, typename _Epilogue>
 __global__ void cutlassGemmKernel_bf16(

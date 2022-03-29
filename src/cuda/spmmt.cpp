e1d88baa1ffe996681cde298188ece4e12bd4212
apuaaChen <chenzd15thu@gmail.com>
2022-03-29 12:12:01 -0700

    [spmmt] go back. let's start with ntn kernel

--- src/cuda/spmmt.cpp
+++ src/cuda/spmmt.cpp
@@ -27,20 +27,7 @@ torch::Tensor spmmt_f16_ntn(
     return spmmt_f16_ntn_cuda(tensor_a_, tensor_b_, tensor_e_);
 }
 
-torch::Tensor spmmt_bf16_nnn_cuda(
-    torch::Tensor tensor_a_,
-    torch::Tensor tensor_b_);
-
-torch::Tensor spmmt_bf16_nnn(
-    torch::Tensor tensor_a_,
-    torch::Tensor tensor_b_)
-{
-    return spmmt_bf16_nnn_cuda(tensor_a_, tensor_b_);
-}
-
-
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("spmmt_bf16_ntn", &spmmt_bf16_ntn, "Cutlass SpMM bf16 kernel ntn");
     m.def("spmmt_f16_ntn", &spmmt_f16_ntn, "Cutlass SpMM fp16 kernel ntn");
-    m.def("spmmt_bf16_nnn", &spmmt_bf16_nnn, "Cutlass SpMM bf16 kernel nnn");
 }

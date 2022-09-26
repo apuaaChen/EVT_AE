cbae6827791e67ff99469a8418246831dd66b4be
apuaaChen <chenzd15thu@gmail.com>
2022-09-26 01:17:24 +0000

    [common factor] update common factor reduction

---
+++ Codegen/compiler/nodes.py
@@ -0,0 +1,169 @@
+################################################################################
+# This file contains helper functions to inject different kind of node to graph
+# It also infers the data type and shapes without actually running the code
+################################################################################
+import torch
+import torch.fx as fx
+import operator
+
+
+def inject_get_attr(inject_point, module, graph, tensor, tensor_name):
+    # update injection point to maintain topological order
+    graph.inserting_after(inject_point)
+    # register the tensor in the module
+    module.register_buffer(tensor_name, tensor)
+    # create get attribute node
+    return graph.get_attr(tensor_name)
+
+
+def inject_softmax(inject_point, graph, parent_node, dim, half_to_float=False, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+    graph.inserting_after(inject_point)
+    print(parent_node.args)
+    softmax_node = graph.call_function(torch.ops.aten._softmax, args=(tmp_node, dim, half_to_float))
+    softmax_node.meta = {}
+    if half_to_float:
+        softmax_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace(dtype=torch.float32)
+    else:
+        softmax_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace()
+    return softmax_node
+
+def inject_log(inject_point, graph, parent_node, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+    graph.inserting_after(inject_point)
+    print(parent_node.args)
+    log_node = graph.call_function(torch.ops.aten.log, args=(tmp_node,))
+    log_node.meta = {}
+    log_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace()
+    return log_node
+
+def inject_onehot(inject_point, graph, num_classes, parent_node, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+    graph.inserting_after(inject_point)
+    one_hot_node = graph.call_function(torch.ops.aten.one_hot, args=(tmp_node,), kwargs={"num_classes": num_classes})
+    one_hot_node.meta = {}
+    shape = list(parent_node.meta['tensor_meta'].shape)
+    shape.append(num_classes)
+    one_hot_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace(shape=shape)
+    return one_hot_node
+
+def inject_neg(inject_point, graph, parent_node, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+    graph.inserting_after(inject_point)
+    neg_node = graph.call_function(torch.ops.aten.neg, args=(tmp_node,))
+    neg_node.meta = {}
+    neg_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace()
+    return neg_node
+
+
+def get_broadcast_shape(lhs, rhs):
+    if isinstance(lhs, fx.Node):
+        lhs_shape = lhs.meta['tensor_meta'].shape
+    else:
+        lhs_shape = ()
+    if isinstance(rhs, fx.Node):
+        rhs_shape = rhs.meta['tensor_meta'].shape
+    else:
+        rhs_shape = ()
+    if lhs_shape == () and rhs_shape == ():
+        shape = ()
+    elif lhs_shape == () and rhs_shape != ():
+        shape = rhs_shape
+    elif rhs_shape == () and lhs_shape != ():
+        shape = lhs_shape
+    else:
+        shape = tuple([max(l, r) for l, r in zip(list(lhs_shape), list(rhs_shape))])
+    
+    return shape
+
+def get_auto_type_conversion(lhs, rhs):
+    if isinstance(lhs, fx.Node):
+        lhs_dtype = lhs.meta['tensor_meta'].dtype
+    else:
+        lhs_dtype = None
+    if isinstance(rhs, fx.Node):
+        rhs_dtype = rhs.meta['tensor_meta'].dtype
+    else:
+        rhs_dtype = None
+    if lhs_dtype is None and rhs_dtype is None:
+        dtype = None
+    elif lhs_dtype is None and rhs_dtype is not None:
+        dtype = rhs_dtype
+    elif rhs_dtype is None and lhs_dtype is not None:
+        dtype = lhs_dtype
+    else:
+        if lhs_dtype in [torch.int64, torch.int32, torch.int16]:
+            dtype = rhs_dtype
+        else:
+            dtype = lhs_dtype
+    
+    return dtype
+
+def inject_mul(inject_point, graph, lhs, rhs, tmp_lhs=None, tmp_rhs=None):
+    if tmp_lhs is None: tmp_lhs = lhs
+    if tmp_rhs is None: tmp_rhs = rhs
+
+    graph.inserting_after(inject_point)
+    mul_node = graph.call_function(torch.ops.aten.mul, args=(tmp_lhs, tmp_rhs))
+    shape = get_broadcast_shape(lhs, rhs)
+    dtype = get_auto_type_conversion(lhs, rhs)
+    mul_node.meta = {}
+    mul_node.meta['tensor_meta'] = inject_point.meta['tensor_meta']._replace(shape=shape, dtype=dtype)
+    return mul_node
+
+def inject_ne(inject_point, graph, lhs, rhs, tmp_lhs=None, tmp_rhs=None):
+    if tmp_lhs is None: tmp_lhs = lhs
+    if tmp_rhs is None: tmp_rhs = rhs
+
+    graph.inserting_after(inject_point)
+    ne_node = graph.call_function(torch.ops.aten.ne, args=(tmp_lhs, tmp_rhs))
+    shape = get_broadcast_shape(lhs, rhs)
+    ne_node.meta = {}
+    ne_node.meta['tensor_meta'] = inject_point.meta['tensor_meta']._replace(shape=shape, dtype=torch.bool)
+    return ne_node
+
+def inject_unsqueeze(inject_point, graph, parent_node, dim, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+
+    graph.inserting_after(inject_point)
+    unsqueeze_node = graph.call_function(torch.ops.aten.unsqueeze, args=(tmp_node, dim))
+    
+    # get shape
+    shape = list(parent_node.meta['tensor_meta'].shape)
+    if dim < 0:
+        dim = dim + len(shape) + 1
+    shape.insert(dim, 1)
+    unsqueeze_node.meta = {}
+    unsqueeze_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace(
+        shape=shape
+    )
+    
+    return unsqueeze_node
+
+def inject_sum(inject_point, graph, parent_node, dim, tmp_node=None):
+    if tmp_node is None: tmp_node = parent_node
+
+    graph.inserting_after(inject_point)
+    sum_node = graph.call_function(torch.ops.aten.sum, args=(tmp_node, dim))
+    sum_node.meta = {}
+    shape = list(parent_node.meta['tensor_meta'].shape)
+    if dim < 0:
+        dim = dim + len(shape) + 1
+    shape.pop(dim)
+    sum_node.meta['tensor_meta'] = parent_node.meta['tensor_meta']._replace(
+        shape=shape
+    )
+    
+    return sum_node
+
+def inject_sub(inject_point, graph, lhs, rhs, tmp_lhs=None, tmp_rhs=None):
+    if tmp_lhs is None: tmp_lhs = lhs
+    if tmp_rhs is None: tmp_rhs = rhs
+
+    graph.inserting_after(inject_point)
+    sub_node = graph.call_function(torch.ops.aten.sub, args=(tmp_lhs, tmp_rhs))
+    shape = get_broadcast_shape(lhs, rhs)
+    dtype = get_auto_type_conversion(lhs, rhs)
+    sub_node.meta = {}
+    sub_node.meta['tensor_meta'] = inject_point.meta['tensor_meta']._replace(shape=shape, dtype=dtype)
+    return sub_node
--- Codegen/compiler/passes.py
+++ Codegen/compiler/passes.py
@@ -1,7 +1,6 @@
-from cProfile import label
 import torch
 from torch.fx.passes import graph_drawer
-import torch.nn.functional as F
+from nodes import *
 
 
 ################################################################################
@@ -21,21 +20,18 @@ def pass_loss_elimination(module, graph):
         if node.op == "output":
             # loss is at input_node[0]
             loss_node = node.all_input_nodes[0]
-            # locate the insertion place
-            graph.inserting_after(loss_node)
-            # add fake loss as register_buffer
-            module.register_buffer(
-                "_fake_loss_0", 
-                torch.Tensor([1.0,]).to("cuda").requires_grad_().to(torch.float16))
-            # create node
-            fake_loss_node = graph.get_attr("_fake_loss_0")
-            # insert node
+
+            fake_loss_node = inject_get_attr(
+                loss_node, module, graph, 
+                torch.Tensor([1.0,]).to("cuda").requires_grad_().to(torch.float16), 
+                "_fake_loss_0")
+
             loss_node.replace_all_uses_with(fake_loss_node)
     graph.eliminate_dead_code()
     graph.lint()
 
 ################################################################################
-# TODO: pass, break down log softmax, nll_loss_forward, nll_loss_backward, 
+# Graph-level pass to break down log softmax, nll_loss_forward, nll_loss_backward, 
 # _log_softmax_backward_data
 ################################################################################
 def pass_composed_op_breakdown(module, graph):
@@ -45,52 +41,244 @@ def pass_composed_op_breakdown(module, graph):
         if node.op == "call_function":
             if node.target == torch.ops.aten._log_softmax:
                 # break it down into softmax and log
-                graph.inserting_after(node)
-                softmax_node = graph.call_function(torch.ops.aten._softmax, args=node.args)
-                softmax_node.meta = node.meta
-                graph.inserting_after(softmax_node)
-                log_node = graph.call_function(torch.ops.aten.log, args=(softmax_node,))
-                log_node.meta = node.meta
+                softmax_node = inject_softmax(
+                    node, graph, node.args[0], node.args[1], node.args[2])
+                log_node = inject_log(softmax_node, graph, softmax_node)
                 node.replace_all_uses_with(log_node)
             elif node.target == torch.ops.aten.nll_loss_forward:
                 label_node = node.args[1]
             elif node.target == torch.ops.aten.nll_loss_backward:
-                graph.inserting_after(node)
-                one_hot_node = graph.call_function(torch.ops.aten.one_hot, args=(label_node,), kwargs={"num_classes": node.meta["tensor_meta"].shape[1]})
-                one_hot_node.meta = node.meta
-                graph.inserting_after(one_hot_node)
-                neg_node = graph.call_function(torch.ops.aten.neg, args=(one_hot_node,))
-                neg_node.meta = neg_node.meta
-                graph.inserting_after(neg_node)
-                mul_node = graph.call_function(torch.ops.aten.mul, args=(neg_node, node.args[0]))
-                mul_node.meta = neg_node.meta
-
-                # get padding mask
-                graph.inserting_after(mul_node)
-                ne_node = graph.call_function(torch.ops.aten.ne, args=(label_node, node.args[5]))
-                ne_node.meta = label_node.meta
-
-                graph.inserting_after(ne_node)
-                unsqueeze_node = graph.call_function(torch.ops.aten.unsqueeze, args=(ne_node, 1))
-                graph.inserting_after(unsqueeze_node)
-                mul_mask_node = graph.call_function(torch.ops.aten.mul, args=(mul_node, unsqueeze_node))
-                
+                one_hot_node = inject_onehot(node, graph, node.meta["tensor_meta"].shape[1], label_node)
+                neg_node = inject_neg(one_hot_node, graph, one_hot_node)
+                mul_node = inject_mul(neg_node, graph, neg_node, node.args[0])
+                ne_node = inject_ne(mul_node, graph, label_node, node.args[5])
+                unsqueeze_node = inject_unsqueeze(ne_node, graph, ne_node, 1)
+                mul_mask_node = inject_mul(unsqueeze_node, graph, mul_node, unsqueeze_node)
                 node.replace_all_uses_with(mul_mask_node)
             elif node.target == torch.ops.aten._log_softmax_backward_data:
-                # TODO: Expand this operator
-                graph.inserting_after(node)
-                sum_node = graph.call_function(torch.ops.aten.sum, args=(node.args[0], 1))
-                graph.inserting_after(sum_node)
-                unsqueeze_node = graph.call_function(torch.ops.aten.unsqueeze, args=(sum_node, 1))
-                graph.inserting_after(unsqueeze_node)
-                mul_node = graph.call_function(torch.ops.aten.mul, args=(unsqueeze_node, softmax_node))
-                graph.inserting_after(mul_node)
-                sub_node = graph.call_function(torch.ops.aten.sub, args=(node.args[0], mul_node))
+                sum_node = inject_sum(node, graph, node.args[0], 1)
+                unsqueeze_node = inject_unsqueeze(sum_node, graph, sum_node, 1)
+                mul_node = inject_mul(unsqueeze_node, graph, unsqueeze_node, softmax_node)
+                sub_node = inject_sub(mul_node, graph, node.args[0], mul_node)
                 node.replace_all_uses_with(sub_node)
-                # pass
             
     graph.eliminate_dead_code()
     graph.lint()
+
+################################################################################
+# Graph-level pass to merge duplicated nodes
+################################################################################
+def node_equal(node1, node2):
+    if node1.op != node2.op:
+        return False
+    if node1.target != node2.target:
+        return False
+    if node1.args != node2.args:
+        return False
+    return True
+    
+def pass_remove_duplicated_node(module, graph):
+    modified = True
+    while(modified):
+        modified = False
+        for i, u in enumerate(graph.nodes):
+            for j, v in enumerate(graph.nodes):
+                if i == j: continue
+                if node_equal(u, v):
+                    v.replace_all_uses_with(u)
+                    graph.eliminate_dead_code()
+                    modified = True
+                    break
+    
+    graph.lint()
+
+################################################################################
+# Graph-level pass to merge common factor of add / sub operators
+################################################################################
+
+def extract_factor(node):
+    # return numerator and demoninator factors
+    if isinstance(node, float) or isinstance(node, int):
+        return [node,], []
+    if isinstance(node, torch.fx.Node):
+        if node.op == "call_function":
+            # mul & div
+            if node.target == torch.ops.aten.mul:
+                lhs_factor_n, lhs_factor_d = extract_factor(node.args[0])
+                rhs_factor_n, rhs_factor_d = extract_factor(node.args[1])
+                return lhs_factor_n + rhs_factor_n, lhs_factor_d + rhs_factor_d
+            elif node.target == torch.ops.aten.div:
+                lhs_factor_n, lhs_factor_d = extract_factor(node.args[0])
+                rhs_factor_n, rhs_factor_d = extract_factor(node.args[1])
+                return lhs_factor_n + rhs_factor_d, lhs_factor_d + rhs_factor_n
+            elif node.target == torch.ops.aten.neg:
+                factor_n, factor_d = extract_factor(node.args[0])
+                return [-1,] + factor_n, factor_d
+            # transparent nodes
+            elif node.target in [torch.ops.aten.view, torch.ops.aten.sum, torch.ops.aten.expand, torch.ops.aten.unsqueeze]:
+                return extract_factor(node.args[0])
+            else:
+                return [node, ], []
+        elif node.op in ["get_attr", "placeholder"]:
+            return [node, ], []
+    else:
+        print(node)
+        raise TypeError
+
+
+def remove_factor_denominator(node, common_factor):
+    if isinstance(node, torch.fx.Node):
+        if node.op == "call_function":
+            # mul & div
+            if node.target == torch.ops.aten.mul:
+                remove_factor_denominator(node.args[0], common_factor)
+                remove_factor_denominator(node.args[1], common_factor)
+            elif node.target == torch.ops.aten.div:
+                if node.args[1] in common_factor:
+                    node.replace_all_uses_with(node.arg[0])
+                    remove_factor_denominator(node.args[0], common_factor)
+                else:
+                    remove_factor_numerator(node.args[1], common_factor)
+                    remove_factor_denominator(node.args[0], common_factor)
+            elif node.target == torch.ops.aten.neg:
+                if -1 in common_factor:
+                    node.replace_all_uses_with(node.args[0])
+                    remove_factor_denominator(node.args[0])
+            # transparent nodes
+            if node.target in [torch.ops.aten.view, torch.ops.aten.sum, torch.ops.aten.expand, torch.ops.aten.unsqueeze]:
+                return remove_factor_denominator(node.args[0], common_factor)
+
+def remove_factor_numerator(node, common_factor):
+    # This function takes a node and a list of common factors. 
+    # If the node is common factor itself, return True
+    if isinstance(node, torch.fx.Node):
+        if node in common_factor:
+            return True
+        if node.op == "call_function":
+            # mul & div
+            if node.target == torch.ops.aten.mul:
+                if (node.args[0] in common_factor 
+                    and node.args[1] not in common_factor):
+                    #
+                    node.replace_all_uses_with(node.args[1])
+                    return remove_factor_numerator(node.args[1], common_factor)
+                elif (node.args[0] not in common_factor 
+                    and node.args[1] in common_factor):
+                    #
+                    node.replace_all_uses_with(node.args[0])
+                    return remove_factor_numerator(node.args[0], common_factor)
+                elif (node.args[0] in common_factor 
+                    and node.args[1] in common_factor):
+                    # 
+                    raise NotImplementedError
+                else:
+                    lhs_empty = remove_factor_numerator(node.args[0], common_factor)
+                    rhs_empty = remove_factor_numerator(node.args[1], common_factor)
+                    if lhs_empty and not rhs_empty:
+                        node.replace_all_uses_with(node.args[1])
+                    elif rhs_empty and not lhs_empty:
+                        node.replace_all_uses_with(node.args[0])
+                    else:
+                        return (lhs_empty and rhs_empty)
+            elif node.target == torch.ops.aten.div:
+                if node.args[0] in common_factor:
+                    node.replace_input_with(node.args[0], 1)
+                    return remove_factor_denominator(node.args[1], common_factor)
+                else:
+                    lhs_empty = remove_factor_numerator(node.args[0], common_factor)
+                    rhs_empty = remove_factor_denominator(node.args[1], common_factor)
+                    return (lhs_empty and rhs_empty)
+            elif node.target == torch.ops.aten.neg:
+                if -1 in common_factor:
+                    node.replace_all_uses_with(node.args[0])
+                    return remove_factor_numerator(node.args[0], common_factor)
+                else:
+                    return False
+            # transparent nodes
+            elif node.target in [torch.ops.aten.view, torch.ops.aten.sum, torch.ops.aten.expand, torch.ops.aten.unsqueeze]:
+                return remove_factor_numerator(node.args[0], common_factor)
+            
+def intersection(lst1, lst2):
+    return [value for value in lst1 if value in lst2]
+
+def pass_merge_common_factor(module, graph):
+    tmp_node = None
+    print(dir(graph.nodes))
+    for idx, node in enumerate(graph.nodes):
+        if idx == 0:
+            tmp_node = node
+        if node.op == "call_function":
+            if node.target == torch.ops.aten.add:
+                # extract factors of lhs child and rhs child
+                lhs_factors_n, lhs_factors_d = extract_factor(node.args[0])
+                rhs_factors_n, rhs_factors_d = extract_factor(node.args[1])
+                common_numerator_n = intersection(lhs_factors_n, rhs_factors_n)
+                remove_factor_numerator(node.args[0], [common_numerator_n[0], common_numerator_n[1], common_numerator_n[2]])
+                remove_factor_numerator(node.args[1], [common_numerator_n[0], common_numerator_n[1], common_numerator_n[2]])
+
+                injecting_node = node
+
+                # # inject common factor after the node
+                for factor in common_numerator_n:
+                    print(factor)
+                    if factor == -1:
+                        print("get neg!!!!")
+                        neg_node = inject_neg(injecting_node, graph, injecting_node, tmp_node)
+                        injecting_node.replace_all_uses_with(neg_node)
+                        neg_node.replace_input_with(tmp_node, injecting_node)
+                        injecting_node = neg_node
+                    else:
+                        # check dimension match
+                        injecting_node_shape = list(
+                            injecting_node.meta['tensor_meta'].shape)
+                        if isinstance(factor, fx.Node):
+                            factor_shape = list(
+                                factor.meta['tensor_meta'].shape)
+                        else:
+                            factor_shape = []
+                        if (len(factor_shape) == len(injecting_node_shape)
+                            or len(factor_shape) == 0
+                            or len(injecting_node_shape) == 0):
+                            mul_node = inject_mul(injecting_node, graph, injecting_node, factor, tmp_lhs=tmp_node)
+                            injecting_node.replace_all_uses_with(mul_node)
+                            mul_node.replace_input_with(tmp_node, injecting_node)
+                            injecting_node = mul_node
+                        else:
+                            # get the dimensions to unsqueeze
+                            unsqueeze_dims = []
+                            factor_idx = 0
+                            for idx, injecting_dim in enumerate(injecting_node_shape):
+                                factor_dim = factor_shape[factor_idx]
+                                if (factor_dim != injecting_dim
+                                    and factor_dim != 1 and injecting_dim != 1):
+                                    unsqueeze_dims.append(idx)
+                            
+                            # inject unsqueeze node
+                            unsqueeze_node = injecting_node
+                            for dim in unsqueeze_dims:
+                                unsqueeze_node = inject_unsqueeze(unsqueeze_node, graph, factor, dim)
+
+                            # inject mul node
+                            mul_node = inject_mul(unsqueeze_node, graph, injecting_node, unsqueeze_node, tmp_lhs=tmp_node)
+                            injecting_node.replace_all_uses_with(mul_node)
+                            mul_node.replace_input_with(tmp_node, injecting_node)
+                            injecting_node = mul_node
+    
+    graph.eliminate_dead_code()
+    graph.lint()
+
+
+
+################################################################################
+# Graph-level pass to update data type of module attributes
+################################################################################
+
+def pass_update_attributes(module, graph):
+    for node in graph.nodes:
+        if node.op == "get_attr":
+            attr = node.target
+            setattr(module, attr, getattr(module, attr).to(torch.float16).to("cuda"))
+
 
 def pre_partition_optimization(joint_module):
     # get graph
@@ -100,6 +288,15 @@ def pre_partition_optimization(joint_module):
 
     # pass: composed op breakdown
     pass_composed_op_breakdown(joint_module, graph)
+
+    # pass: remove duplicated nodes
+    pass_remove_duplicated_node(joint_module, graph)
+
+    # pass: merge common factor of add / sub operators
+    pass_merge_common_factor(joint_module, graph)
+
+    # pass: update attributes
+    pass_update_attributes(joint_module, graph)
     
     # recompile graph
     joint_module.recompile()

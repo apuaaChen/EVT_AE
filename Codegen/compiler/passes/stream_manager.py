e109173d52920ae3fb7b98534df1e29621aad69d
apuaaChen <chenzd15thu@gmail.com>
2023-01-11 18:14:52 -0800

    update bert script

--- Codegen/compiler/passes/stream_manager.py
+++ Codegen/compiler/passes/stream_manager.py
@@ -34,128 +34,202 @@ def get_priority(node):
 ################################################################################
 # Graph-level pass to assign streams to different threads to allow streaming
 ################################################################################
-def pass_assign_stream(module, graph,):
+# def pass_assign_stream(module, graph,):
+#     default_stream = torch.cuda.default_stream()
+
+#     less_priority_streams = [
+#         torch.cuda.Stream(priority=0), 
+#         torch.cuda.Stream(priority=0),
+#         torch.cuda.Stream(priority=0)]
+
+#     remaining_nodes = []
+#     executed_nodes = []
+#     wavefronts = []
+
+#     # step 0: get all nodes
+#     for node in graph.nodes:
+#         remaining_nodes.append(node)
+
+#     while len(remaining_nodes) > 0:
+
+#         wavefront = []
+
+#         for node in remaining_nodes:
+#             executable = True
+#             for arg in node.all_input_nodes:
+#                 if arg in wavefront:
+#                     executable = False
+#                 if arg not in executed_nodes:
+#                     executable = False
+            
+#             if executable:
+#                 wavefront.append(node)
+#                 executed_nodes.append(node)
+        
+#         remaining_nodes = [n for n in remaining_nodes if n not in executed_nodes]
+#         wavefronts.append(wavefront)
+#     idx = 0
+#     for wavefront in wavefronts:
+#         for node in wavefront:
+#             if node.op in ["placeholder", "get_attr"]:
+#                 node.meta["stream"] = default_stream
+#             elif node.op == "call_function":
+#                 if node.target in [torch.ops.aten.view, torch.ops.aten.unsqueeze, operator.getitem, torch.ops.aten.permute, torch.ops.aten.squeeze, torch.ops.aten._unsafe_view]:
+#                     node.meta["stream"] = node.all_input_nodes[0].meta["stream"]
+#                 else:
+#                     stream = None
+#                     if len(node.all_input_nodes) == 1:
+#                         if len(node.all_input_nodes[0].users) == 1:
+#                             stream = node.all_input_nodes[0].meta["stream"]
+#                     if stream is None:
+#                         priority = get_priority(node)
+#                         if priority == 0:
+#                             stream = less_priority_streams[idx]
+#                             idx = (idx + 1) % len(less_priority_streams)
+#                         else:
+#                             # stream = torch.cuda.Stream(priority=priority)
+#                             stream = default_stream# high_priority_stream
+
+#                     node.meta["stream"] = stream
+#                     # class CallFunctionWithStream:
+#                     #     __name__ = node.target.__name__
+#                     #     def __init__(self, node) -> None:
+#                     #         # get all input streams
+#                     #         self.input_streams = []
+#                     #         for input_node in node.all_input_nodes:
+#                     #             self.input_streams.append(input_node.meta["stream"])
+#                     #         # get self stream
+#                     #         self.stream = node.meta["stream"]
+#                     #         self.target = node.target
+                        
+#                     #     def __call__(self, *args, **kwargs):
+#                     #         if self.stream == less_priority_stream:
+#                     #             self.stream.wait_stream(high_priority_stream)
+#                     #         # for input_stream in self.input_streams:
+#                     #         #     self.stream.wait_stream(input_stream)
+#                     #         with torch.cuda.stream(self.stream):  
+#                     #             # print(node.target)          
+#                     #             return self.target(*args, **kwargs)
+#                     if hasattr(node.target, "stream"):
+#                         if priority == 0:
+#                             node.target.stream = stream
+#                     # else:
+#                     #     node.target = CallFunctionWithStream(node)
+
+#             elif node.op == "output":
+#                 # inject the final synchronization node
+#                 node.meta["stream"] = default_stream
+
+#                 class OutputSyncOp:
+#                     __name__ = "output_stream_sync"
+#                     def __init__(self, node) -> None:
+#                         self.stream = torch.cuda.current_stream()
+
+
+#                         # get self stream
+#                         self.stream = node.meta["stream"]
+#                         self.input_nodes = []
+#                         self.input_indices = []
+
+#                         for idx, input_node in enumerate(node.all_input_nodes):
+#                             if input_node.meta["stream"] != self.stream:
+#                                 self.input_nodes.append(input_node)
+#                                 self.input_indices.append(idx)
+
+#                     def __call__(self, *args):
+#                         for stream in less_priority_streams:
+#                             torch.cuda.current_stream().wait_stream(stream)
+#                         return args
+                
+#                 graph.inserting_before(node)
+#                 output_sync = OutputSyncOp(node)
+#                 input_args = []
+#                 for idx in output_sync.input_indices:
+#                     input_args.append(list(node.all_input_nodes)[idx])
+#                 sync_node = graph.call_function(output_sync, args=tuple(input_args))
+
+#                 graph.inserting_after(sync_node)
+#                 for idx, output_node in enumerate(output_sync.input_nodes):
+#                     get_item_node = graph.call_function(operator.getitem, args=(sync_node, idx))
+#                     graph.inserting_after(get_item_node)
+
+#                     node.replace_input_with(output_node, get_item_node)
+#             # else:
+#             #     print(node)
+#                 # raise NotImplementedError()
+#     graph.lint()
+
+
+def pass_assign_stream(module, graph):
     default_stream = torch.cuda.default_stream()
 
     less_priority_streams = [
         torch.cuda.Stream(priority=0), 
         torch.cuda.Stream(priority=0),
-        torch.cuda.Stream(priority=0)]
+        torch.cuda.Stream(priority=0)
+    ]
 
-    remaining_nodes = []
-    executed_nodes = []
-    wavefronts = []
-
-    # step 0: get all nodes
-    for node in graph.nodes:
-        remaining_nodes.append(node)
-
-    while len(remaining_nodes) > 0:
-
-        wavefront = []
-
-        for node in remaining_nodes:
-            executable = True
-            for arg in node.all_input_nodes:
-                if arg in wavefront:
-                    executable = False
-                if arg not in executed_nodes:
-                    executable = False
-            
-            if executable:
-                wavefront.append(node)
-                executed_nodes.append(node)
-        
-        remaining_nodes = [n for n in remaining_nodes if n not in executed_nodes]
-        wavefronts.append(wavefront)
     idx = 0
-    for wavefront in wavefronts:
-        for node in wavefront:
-            if node.op in ["placeholder", "get_attr"]:
-                node.meta["stream"] = default_stream
-            elif node.op == "call_function":
-                if node.target in [torch.ops.aten.view, torch.ops.aten.unsqueeze, operator.getitem, torch.ops.aten.permute, torch.ops.aten.squeeze, torch.ops.aten._unsafe_view]:
-                    node.meta["stream"] = node.all_input_nodes[0].meta["stream"]
-                else:
-                    stream = None
-                    if len(node.all_input_nodes) == 1:
-                        if len(node.all_input_nodes[0].users) == 1:
-                            stream = node.all_input_nodes[0].meta["stream"]
-                    if stream is None:
-                        priority = get_priority(node)
-                        if priority == 0:
-                            stream = less_priority_streams[idx]
-                            idx = (idx + 1) % len(less_priority_streams)
-                        else:
-                            # stream = torch.cuda.Stream(priority=priority)
-                            stream = default_stream# high_priority_stream
+    for node in graph.nodes:
+        if node.op in ["placeholder", "get_attr"]:
+            node.meta["stream"] = default_stream
+        elif node.op == "call_function":
+            if node.target in [torch.ops.aten.view, torch.ops.aten.unsqueeze, operator.getitem, torch.ops.aten.permute, torch.ops.aten.squeeze, torch.ops.aten._unsafe_view]:
+                node.meta["stream"] = node.all_input_nodes[0].meta["stream"]
+            else:
+                stream = None
+                if len(node.all_input_nodes) == 1:
+                    if len(node.all_input_nodes[0].users) == 1:
+                        stream = node.all_input_nodes[0].meta["stream"]
+                if stream is None:
+                    priority = get_priority(node)
+                    if priority == 0:
+                        stream = less_priority_streams[idx]
+                        idx = (idx + 1) % len(less_priority_streams)
+                    else:
+                        stream = default_stream
 
-                    node.meta["stream"] = stream
-                    # class CallFunctionWithStream:
-                    #     __name__ = node.target.__name__
-                    #     def __init__(self, node) -> None:
-                    #         # get all input streams
-                    #         self.input_streams = []
-                    #         for input_node in node.all_input_nodes:
-                    #             self.input_streams.append(input_node.meta["stream"])
-                    #         # get self stream
-                    #         self.stream = node.meta["stream"]
-                    #         self.target = node.target
-                        
-                    #     def __call__(self, *args, **kwargs):
-                    #         if self.stream == less_priority_stream:
-                    #             self.stream.wait_stream(high_priority_stream)
-                    #         # for input_stream in self.input_streams:
-                    #         #     self.stream.wait_stream(input_stream)
-                    #         with torch.cuda.stream(self.stream):  
-                    #             # print(node.target)          
-                    #             return self.target(*args, **kwargs)
-                    if hasattr(node.target, "stream"):
-                        if priority == 0:
-                            node.target.stream = stream
-                    # else:
-                    #     node.target = CallFunctionWithStream(node)
+                node.meta["stream"] = stream
+                if hasattr(node.target, "stream"):
+                    if priority == 0:
+                        node.target.stream = stream
+        
+        elif node.op == "output":
+            # inject the final synchronization node
+            node.meta["stream"] = default_stream
 
-            elif node.op == "output":
-                # inject the final synchronization node
-                node.meta["stream"] = default_stream
-
-                class OutputSyncOp:
-                    __name__ = "output_stream_sync"
-                    def __init__(self, node) -> None:
-                        self.stream = torch.cuda.current_stream()
+            class OutputSyncOp:
+                __name__ = "output_stream_sync"
+                def __init__(self, node) -> None:
+                    self.stream = torch.cuda.current_stream()
 
 
-                        # get self stream
-                        self.stream = node.meta["stream"]
-                        self.input_nodes = []
-                        self.input_indices = []
+                    # get self stream
+                    self.stream = node.meta["stream"]
+                    self.input_nodes = []
+                    self.input_indices = []
 
-                        for idx, input_node in enumerate(node.all_input_nodes):
-                            if input_node.meta["stream"] != self.stream:
-                                self.input_nodes.append(input_node)
-                                self.input_indices.append(idx)
+                    for idx, input_node in enumerate(node.all_input_nodes):
+                        if input_node.meta["stream"] != self.stream:
+                            self.input_nodes.append(input_node)
+                            self.input_indices.append(idx)
 
-                    def __call__(self, *args):
-                        for stream in less_priority_streams:
-                            torch.cuda.current_stream().wait_stream(stream)
-                        return args
-                
-                graph.inserting_before(node)
-                output_sync = OutputSyncOp(node)
-                input_args = []
-                for idx in output_sync.input_indices:
-                    input_args.append(list(node.all_input_nodes)[idx])
-                sync_node = graph.call_function(output_sync, args=tuple(input_args))
+                def __call__(self, *args):
+                    for stream in less_priority_streams:
+                        torch.cuda.current_stream().wait_stream(stream)
+                    return args
+            
+            graph.inserting_before(node)
+            output_sync = OutputSyncOp(node)
+            input_args = []
+            for idx in output_sync.input_indices:
+                input_args.append(list(node.all_input_nodes)[idx])
+            sync_node = graph.call_function(output_sync, args=tuple(input_args))
 
-                graph.inserting_after(sync_node)
-                for idx, output_node in enumerate(output_sync.input_nodes):
-                    get_item_node = graph.call_function(operator.getitem, args=(sync_node, idx))
-                    graph.inserting_after(get_item_node)
+            graph.inserting_after(sync_node)
+            for idx, output_node in enumerate(output_sync.input_nodes):
+                get_item_node = graph.call_function(operator.getitem, args=(sync_node, idx))
+                graph.inserting_after(get_item_node)
 
-                    node.replace_input_with(output_node, get_item_node)
-            # else:
-            #     print(node)
-                # raise NotImplementedError()
+                node.replace_input_with(output_node, get_item_node)
     graph.lint()
-
--- benchmark/bert/bert_cuda_graph_v2.py
+++ benchmark/bert/bert_cuda_graph_v2.py
@@ -15,12 +15,30 @@ pycutlass.compiler.nvcc()
 import nvtx
 
 
-i = 2
-input_ids = torch.load("/workspace/bert/batch/input_ids_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
-token_type_ids = torch.load("/workspace/bert/batch/token_type_ids_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
-attention_mask = torch.load("/workspace/bert/batch/attention_mask_iter%d.pt"%i).to(torch.float16).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
-next_sentence_labels = torch.load("/workspace/bert/batch/next_sentence_labels_iter%d.pt"%i).unsqueeze(0).expand([4, 8]).contiguous().view([-1]).to("cuda")
-labels = torch.load("/workspace/bert/batch/labels_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
+import argparse
+parser = argparse.ArgumentParser(description="Bert End-to-End Training with CUDA Graph")
+# Hyper-parameter that defines the model size
+parser.add_argument('--batch_size', '-b', type=int, default=32, help="Training batch size per GPU")
+parser.add_argument('--seq_len', '-l', type=int, default=512, help="Sequence length")
+# Modes
+parser.add_argument('--mode', '-m', type=str, default="verify", choices=["verify", "profile"])
+args = parser.parse_args()
+
+# creating inputs
+
+input_ids = torch.randint(low=101, high=29858, size=(args.batch_size, args.seq_len), dtype=torch.int64, device="cuda")
+token_type_ids = torch.randint(low=0, high=2, size=(args.batch_size, args.seq_len), dtype=torch.int64, device="cuda")
+attention_mask = torch.ones(size=(args.batch_size, args.seq_len), dtype=torch.float16, device="cuda")
+next_sentence_labels = torch.randint(low=0, high=2, size=(args.batch_size,), dtype=torch.int64, device="cuda")
+labels = torch.randint(low=-1, high=26555, size=(args.batch_size, args.seq_len), dtype=torch.int64, device="cuda")
+
+# Also supports loading real data loaded from dataset
+# i = 2
+# input_ids = torch.load("/workspace/bert/batch/input_ids_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
+# token_type_ids = torch.load("/workspace/bert/batch/token_type_ids_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
+# attention_mask = torch.load("/workspace/bert/batch/attention_mask_iter%d.pt"%i).to(torch.float16).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
+# next_sentence_labels = torch.load("/workspace/bert/batch/next_sentence_labels_iter%d.pt"%i).unsqueeze(0).expand([4, 8]).contiguous().view([-1]).to("cuda")
+# labels = torch.load("/workspace/bert/batch/labels_iter%d.pt"%i).unsqueeze(0).expand([4, 8, 512]).contiguous().view([-1, 512]).to("cuda")
 
 batch = {
     "input_ids": input_ids,
@@ -29,7 +47,6 @@ batch = {
     "next_sentence_labels": next_sentence_labels,
     "labels": labels
 }
-
 
 ## args
 config_file = "./large.json" 
@@ -71,7 +88,7 @@ def prepare_model_and_optimizer(device, sequence_output_is_dense, reference=None
 
 ## create model and optimizer
 model, optimizer = prepare_model_and_optimizer("cuda", sequence_output_is_dense=False)
-model_fused, optimizer_fused = prepare_model_and_optimizer("cuda", sequence_output_is_dense=False, reference=model)
+
 
 ## amp initialize
 model, optimizer = amp.initialize(
@@ -81,6 +98,35 @@ model, optimizer = amp.initialize(
     loss_scale="dynamic"
 )
 
+# 
+model.train()
+
+optimizer.zero_grad()
+loss = model(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+with scale_loss(loss, optimizer) as scaled_loss:
+    scaled_loss.backward()
+
+if args.mode == "profile":
+    for i in range(10):
+        loss = model(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+        with scale_loss(loss, optimizer) as scaled_loss:
+            scaled_loss.backward()
+
+    with nvtx.annotate("pytorch 40 iter"):
+        for i in range(40):
+            with nvtx.annotate("torch"):
+                loss = model(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+                with scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+
+    torch.cuda.synchronize()
+
+model_fused, optimizer_fused = prepare_model_and_optimizer("cuda", sequence_output_is_dense=False, reference=model)
+
+if args.mode == "profile":
+    del model
+    del optimizer
+
 model_fused, optimizer_fused = amp.initialize(
     model_fused, optimizer_fused, 
     cast_model_outputs=torch.float16, 
@@ -88,48 +134,36 @@ model_fused, optimizer_fused = amp.initialize(
     loss_scale="dynamic"
 )
 
-# 
-model.train()
 model_fused.train()
-
 model_fused.aot_optimize(compiler_fn, compiler_fn, partition_func)
-# loss = model_fused(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
-# with scale_loss(loss, optimizer_fused) as scaled_loss:
-#     scaled_loss.backward()
-model_fused.capture_graph(batch=32, sequence_length=512, optimizer=optimizer_fused)
+model_fused.capture_graph(batch=args.batch_size, sequence_length=args.seq_len, optimizer=optimizer_fused)
 
-
-## Functional verification
-optimizer.zero_grad()
-loss = model(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
-with scale_loss(loss, optimizer) as scaled_loss:
-    scaled_loss.backward()
 
 optimizer_fused.zero_grad()
-# loss = model_fused(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
-# with scale_loss(loss, optimizer_fused) as scaled_loss:
-#     scaled_loss.backward()
-model_fused.training_with_graph(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+s = torch.cuda.Stream(priority=-1)
+s.wait_stream(torch.cuda.current_stream())
+with torch.cuda.stream(s):
+    model_fused.training_with_graph(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
 
-for param1, param2 in zip(list(model.named_parameters()), list(model_fused.named_parameters())):
-    print(torch.sum(torch.isclose(param1[1].grad, param2[1].grad, rtol=1e-1)) / param2[1].grad.numel())
-    try:
-        assert torch.sum(torch.isclose(param1[1].grad, param2[1].grad, rtol=1e-1)) / param2[1].grad.numel() > 0.9
-    except:
-        print(param1[0])
-        print(param1[1].grad)
-        print(param2[1].grad)
-        print(param1[1].grad.size())
+if args.mode == "profile":
+    s = torch.cuda.Stream(priority=-1)
+    s.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(s):
+        for i in range(10):
+            model_fused.training_with_graph(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+    
+        with nvtx.annotate("ours 40 iter"):
+            for i in range(40):
+                with nvtx.annotate("fused"):
+                    model_fused.training_with_graph(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
 
-# for i in range(40):
-#     with nvtx.annotate("torch"):
-#         loss = model(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
-#         with scale_loss(loss, optimizer) as scaled_loss:
-#             scaled_loss.backward()
-
-# s = torch.cuda.Stream(priority=-1)
-# s.wait_stream(torch.cuda.current_stream())
-# with torch.cuda.stream(s):
-#     for i in range(40):
-#         with nvtx.annotate("fused"):
-#             model_fused.training_with_graph(input_ids, token_type_ids, attention_mask, labels, labels, next_sentence_labels)
+if args.mode == "verify":
+    for param1, param2 in zip(list(model.named_parameters()), list(model_fused.named_parameters())):
+        print(torch.sum(torch.isclose(param1[1].grad, param2[1].grad, rtol=1e-1)) / param2[1].grad.numel())
+        try:
+            assert torch.sum(torch.isclose(param1[1].grad, param2[1].grad, rtol=1e-1)) / param2[1].grad.numel() > 0.9
+        except:
+            print(param1[0])
+            print(param1[1].grad)
+            print(param2[1].grad)
+            print(param1[1].grad.size())

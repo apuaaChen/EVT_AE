4ba53709415ef4e8360c565a065b110bc0cb9c9f
apuaaChen <chenzd15thu@gmail.com>
2023-05-16 13:17:43 -0700

    update docker file

--- script/docker/launch.sh
+++ script/docker/launch.sh
@@ -1,6 +1,4 @@
-docker run --gpus all --name zdchen_nv_torch_5 \
- -v /data/datasets/users/zdchen/workspace/bert:/workspace/bert \
- -v ~/projects/sparseTraining:/workspace/sparseTraining \
- -v ~/projects/cutlass:/workspace/cutlass \
- -v /data:/data \
- -it -p 8225:22 nv_torch
+docker run --gpus all --name $1 \
+ -v /data:/data -v \
+ ../../../SEAL-PICASO-ML-Compiler:/workspace/SEAL-PICASO-ML-Compiler -it \
+ -p $2:22 mlcompiler

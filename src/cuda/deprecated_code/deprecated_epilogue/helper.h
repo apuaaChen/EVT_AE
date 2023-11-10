75ed6023fb58f3a14da9a6bd371defe5e06ab3c1
apuaaChen <chenzd15thu@gmail.com>
2023-11-10 04:35:15 +0000

    deprecate old implementation

--- src/cuda/deprecated_code/deprecated_epilogue/helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef EPILOGUE_HELPER_H
-#define EPILOGUE_HELPER_H
-#include <stdio.h>
-
-__device__ void print_val(int blockidx, int blockidy, int blockidz, int threadid, float value){
-    if (blockidx == blockIdx.x && blockidy == blockIdx.y && blockidz == blockIdx.z && threadid == threadIdx.x) printf("tid: %d, value is: %.8f\n", threadid, float(value));
-}
-
-#endif
